f64c6412c149ae1c54858ab45bcd7fb1f601c3c8
ronak-ravtode <ronakravtode001@gmail.com>
2025-05-25 17:01:33 +0530

    Array is Sorted or not

---
+++ Recursion/ArraySortedOrNot.java
@@ -0,0 +1,10 @@
+public class ArraySortedOrNot {
+    public static boolean isSorted(int arr[],int i){
+        if(i==arr.length-1) return true;
+        if(arr[i]>arr[i+1]) return false;
+        return isSorted(arr, i+1);
+    }
+    public static void main(String[] args) {
+        System.out.println(isSorted(new int[]{1,2,3,4,5},0));
+    }
+}
