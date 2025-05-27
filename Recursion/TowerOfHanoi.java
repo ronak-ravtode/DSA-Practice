3d27c360835e0173f8362a1c19a5854e217c942a
ronak-ravtode <ronakravtode001@gmail.com>
2025-05-27 13:27:13 +0530

    Towe of Hanoi

---
+++ Recursion/TowerOfHanoi.java
@@ -0,0 +1,14 @@
+public class TowerOfHanoi {
+    public static void StepForPowerOfHanoi(int n,char s,char h,char d){
+        if(n==1){
+            System.out.println(s+"->"+d);
+            return;
+        }
+        StepForPowerOfHanoi(n-1, s, d, h);
+        System.out.println(s+"->"+d);
+        StepForPowerOfHanoi(n-1, h, s, d);   
+    }
+    public static void main(String[] args) {
+        StepForPowerOfHanoi(3,'A','B','C');
+    }
+}
